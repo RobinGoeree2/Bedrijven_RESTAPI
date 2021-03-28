9956915cec4f9e24d6d63851f9b2d97f16b05309
Robin Goeree <56107687+RobinGoeree2@users.noreply.github.com>
2021-03-28 21:38:40 +0200

    Reverted Revert

--- RESTAPI/src/main/java/com/company/RESTAPI/RestapiApplication.java
+++ RESTAPI/src/main/java/com/company/RESTAPI/RestapiApplication.java
@@ -10,5 +10,32 @@ public class RestapiApplication {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
+<<<<<<< HEAD
 	
+=======
+	//@GetMapping("/hello")
+	//public String hello(@RequestParam(value = "name", defaultValue = "World") String name)
+	//{
+	//	return String.format("Hello %s!", name);
+	//}
+
+	@GetMapping("/restapi/v1/getperson")
+	public Person getPerson()
+	{
+		return this.persoon;
+	}
+
+	@PostMapping
+	@RequestMapping("restapi/v1/addperson")
+	public void addPerson(@RequestBody Person person)
+	{
+		this.persoon = person;
+	}
+
+	@DeleteMapping("restapi/v1/deleteperson")
+	public void deletePerson()
+	{
+		this.persoon = null;
+	}
+>>>>>>> parent of 90e84e2 (Revert "Added delete functionality")
 }
