ea27ff0dbfd8beaa86adf34064408ca6f67599a3
Robin Goeree <56107687+RobinGoeree2@users.noreply.github.com>
2021-03-28 21:49:25 +0200

    API back to normal

--- RESTAPI/src/main/java/com/company/RESTAPI/RestapiApplication.java
+++ RESTAPI/src/main/java/com/company/RESTAPI/RestapiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class RestapiApplication
 {
 	private Person persoon;
