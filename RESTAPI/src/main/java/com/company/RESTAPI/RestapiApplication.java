e5f287f12ff37464a10ca99596fe615acef02db6
Robin Goeree <56107687+RobinGoeree2@users.noreply.github.com>
2021-03-28 21:42:59 +0200

    Somehow fixed main file issue

--- RESTAPI/src/main/java/com/company/RESTAPI/RestapiApplication.java
+++ RESTAPI/src/main/java/com/company/RESTAPI/RestapiApplication.java
@@ -2,17 +2,18 @@ package com.company.RESTAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication
+{
+	private Person persoon;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
-<<<<<<< HEAD
-	
-=======
 	//@GetMapping("/hello")
 	//public String hello(@RequestParam(value = "name", defaultValue = "World") String name)
 	//{
@@ -37,5 +38,4 @@ public class RestapiApplication {
 	{
 		this.persoon = null;
 	}
->>>>>>> parent of 90e84e2 (Revert "Added delete functionality")
 }
